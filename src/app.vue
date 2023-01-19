6adcb4f931b06b3e59b606a9cfcebda2e75a6892
Robert Møller <rober@hjortsholm.com>
2023-01-19 10:53:30 +0100

    trying to get pwa to work

--- src/app.vue
+++ src/app.vue
@@ -1,21 +1,13 @@
 <script lang="ts" setup>
-import {workbox} from './rgisterServiceWorker'
 import Navigation from "@/components/navigation.vue";
 import {network, networkOf} from "@/models/network";
 import {VOffline} from "v-offline";
 
-workbox.addEventListener('waiting', () => {
-  workbox.addEventListener('controlling', () => {
-    window.location.reload();
-  });
-  workbox.messageSW({type: 'SKIP_WAITING'});
-});
-
 const onNetworkChange = (status: boolean) => {
   network.value = networkOf(status)
 }
-
 </script>
+
 
 <template>
   <VOffline @detected-condition="onNetworkChange"></VOffline>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import {createApp} from 'vue'
 import {VueFire, VueFireAuth} from 'vuefire'
-import {VOffline} from 'v-offline'
 import {firebaseApp} from './firebase'
 import {router} from "./routes"
 import {md} from "@/directives/md";
 import {date} from "@/directives/date";
 import App from './app.vue'
+import "./registerServiceWorker";
 import './assets/main.css'
 import '@picocss/pico'
 import {library} from '@fortawesome/fontawesome-svg-core'
