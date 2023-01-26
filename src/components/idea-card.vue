8ce3b939d6c3a18f98798456d521b88bffec9eae
Samuel Mazaud <samuelmazaud@protonmail.com>
2023-01-26 05:06:15 -0500

    Delete button and favicons

--- src/components/idea-card.vue
+++ src/components/idea-card.vue
@@ -1,35 +1,37 @@
 <script setup>
-import {SavedIdea} from "@/models/idea";
-import {computed} from "vue";
-import {useRouter} from "vue-router";
-import {useCurrentUser, useDocument} from "vuefire";
-import {deleteDoc, doc, setDoc} from 'firebase/firestore'
-import {NewVote, voteConverter} from "@/models/vote";
-import {ideasRef} from "@/firebase";
+import { SavedIdea } from "@/models/idea";
+import { computed } from "vue";
+import { useRouter } from "vue-router";
+import { useCurrentUser, useDocument } from "vuefire";
+import { deleteDoc, doc, setDoc } from "firebase/firestore";
+import { NewVote, voteConverter } from "@/models/vote";
+import { ideasRef } from "@/firebase";
 import ProfileName from "@/components/profile-name.vue";
-import {isOffline, isOnline} from "@/models/network";
-
+import { isOffline, isOnline } from "@/models/network";
 
 const props = defineProps({
   idea: {
     type: SavedIdea,
-    required: true
+    required: true,
   },
   index: {
     type: Number,
-    default: -1
-  }
+    default: -1,
+  },
 });
 const router = useRouter();
 const user = useCurrentUser();
 const isAuthenticated = computed(() => user.value !== null);
 
-const voteRef = user.value ? doc(ideasRef, props.idea.id, "votes", user.value.uid)
-    .withConverter(voteConverter) : null;
+const voteRef = user.value
+  ? doc(ideasRef, props.idea.id, "votes", user.value.uid).withConverter(
+      voteConverter
+    )
+  : null;
 const vote = user.value ? useDocument(voteRef) : null;
 
-const podium = ['🥇', '🥈', '🥉']
-const upvoteIdea = async v => {
+const podium = ["🥇", "🥈", "🥉"];
+const upvoteIdea = async (v) => {
   if (user) {
     if (v) {
       await deleteDoc(voteRef);
@@ -38,49 +40,82 @@ const upvoteIdea = async v => {
       await setDoc(voteRef, newVote);
     }
   }
-}
-
+};
 
 const goToIdea = () => {
   router.push(`/idea/${props.idea.id}`);
-}
+};
+
+const deleteIdea = async () => {
+  if (window.confirm("Are you sure you want to delete this idea?")) {
+    await deleteDoc(ideasRef.doc(props.idea.id));
+  }
+};
+let onProfilePage = false;
+
+const checkProfilePage = async () => {
+  if (window.location.href.includes("/profile/")) {
+    onProfilePage = true;
+  } else {
+    onProfilePage = false;
+  }
+};
+
+checkProfilePage();
+
+const onProfilePageCheck = computed(() => onProfilePage);
 </script>
 
 <template>
   <article :key="idea.id" @click="goToIdea">
     <hgroup>
-      <h2>{{ idea.title }} {{ 0 <= props.index < podium.length ? podium[props.index] : "" }}</h2>
+      <h2>
+        {{ idea.title }}
+        {{ 0 <= props.index < podium.length ? podium[props.index] : "" }}
+      </h2>
       <div class="context">
         <p>
           <a :href="'/profile/' + idea.author">
-            <ProfileName :uid="idea.author"/>
+            <ProfileName :uid="idea.author" />
           </a>
         </p>
 
         <div class="actions">
-          <span v-if="isOnline"
-                :class="user && vote?.upvote ? 'secondary' :'primary'"
-                :data-tooltip="isOnline
-                  ? vote?.upvote ? 'remove vote': 'upvote'
-                  : 'connect to upvote'"
-                href="#" :role="isAuthenticated ?'button':''"
-                :disabled="isOffline ? true : null"
-                @click.stop="upvoteIdea(vote)">
+          <span
+            v-if="isOnline"
+            :class="user && vote?.upvote ? 'secondary' : 'primary'"
+            :data-tooltip="
+              isOnline
+                ? vote?.upvote
+                  ? 'remove vote'
+                  : 'upvote'
+                : 'connect to upvote'
+            "
+            href="#"
+            :role="isAuthenticated ? 'button' : ''"
+            :disabled="isOffline ? true : null"
+            @click.stop="upvoteIdea(vote)"
+          >
             <span>{{ idea.voteCount }}</span>
             <span>👍</span>
           </span>
 
-
-          <a href="#" role="button"
-             data-tooltip="comment"
-             :disabled="isOffline ? true : null"
-             @click="goToIdea">
+          <a
+            href="#"
+            role="button"
+            data-tooltip="comment"
+            :disabled="isOffline ? true : null"
+            @click="goToIdea"
+          >
             <span>{{ idea.commentCount }}</span>
             <span>💬</span>
           </a>
+
+          <button v-if="onProfilePageCheck" @click="deleteIdea" class="delete">
+            <span>🗑️</span>
+          </button>
         </div>
       </div>
-
     </hgroup>
   </article>
 </template>
@@ -90,13 +125,16 @@ article:hover {
   cursor: pointer;
 }
 
+.delete {
+  background-color: red;
+}
 .actions > * {
   text-decoration: none;
   border-bottom: none !important;
 
   display: flex;
   flex-direction: row;
-  gap: .5rem;
+  gap: 0.5rem;
 }
 
 article {
@@ -126,7 +164,8 @@ hgroup {
   gap: 1rem;
 }
 
-.context h2, p {
+.context h2,
+p {
   margin: 0;
   padding: 0;
 }
